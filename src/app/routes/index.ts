ee5e55de7c4d45996bbf2022895cc07f8732813d
Luis Otávio Lima Caminha <luis.otavio2425@gmail.com>
2023-05-30 12:29:36 -0300

    Adding players routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import usersRouter from './users.routes';
 import cardsRoutes from './cards.routes';
 import historyRoutes from './history.routes';
+import playersRoutes from './players.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/cards', cardsRoutes);
 routes.use('/history', historyRoutes);
+routes.use('/players', playersRoutes);
 
 export default routes;
