e65a2ff752e7ac233b2efd6ae365a18265f010fa
Mateus Fonseca Lima <78103632+mateusxfl@users.noreply.github.com>
2024-03-12 21:22:43 -0300

    feat: changes needed for card suggestion function (#2)

--- src/app/routes/cards.routes.ts
+++ src/app/routes/cards.routes.ts
@@ -8,10 +8,10 @@ const cardsRoutes = Router();
 
 cardsRoutes.get('/:language', CardsController.index);
 cardsRoutes.get('/:language/:id', CardsController.show);
+cardsRoutes.post('/', upload.multer.single('img'), CardsController.store);
 
 cardsRoutes.use(authSession);
 
-cardsRoutes.post('/', upload.multer.single('img'), CardsController.store);
 cardsRoutes.put('/:id', upload.multer.single('img'), CardsController.update);
 cardsRoutes.delete('/:id', CardsController.delete);
 
